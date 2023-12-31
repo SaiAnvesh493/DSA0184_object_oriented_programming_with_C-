96993fa1254b1e66e1d7612cbb826dc449df90da
SaiAnvesh493 <113960097+SaiAnvesh493@users.noreply.github.com>
2023-12-31 10:41:07 +0530

    Add files via upload

---
+++ div_by_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the number:";
+	cin>>n;
+	if(n%3==0)
+	{
+	  cout<<"Number is divisble by 3";	
+	}
+	else
+	{
+		cout<<"Number is Not divisble by 3";
+	}
+	return 0;
+}
